b4ddc55c856b467838354a6a161dce20754bb6e3
Puru80 <puru.agar99@gmail.com>
2025-05-28 13:15:29 +0530

    Removed unwanted comments

--- app/(auth)/auth.config.ts
+++ app/(auth)/auth.config.ts
@@ -7,19 +7,9 @@ export const authConfig = {
     newUser: "/",
   },
   providers: [
-    
+
   ],
   callbacks: {
-    // authorized({ auth, request: { nextUrl } }) {
-    //   const isLoggedIn = !!auth?.user;
-    //   const isOnDashboard = nextUrl.pathname.startsWith("/");
-    //   if (isOnDashboard) {
-    //     if (isLoggedIn) return true;
-    //     return false; // Redirect unauthenticated users to login page
-    //   } else if (isLoggedIn) {
-    //     return Response.redirect(new URL("/", nextUrl));
-    //   }
-    //   return true;
-    // },
+
   },
 } satisfies NextAuthConfig;
