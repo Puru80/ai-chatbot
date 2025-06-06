6bd03e6b3bac8093f21c764da566b56e704d5634
Puru80 <puru.agar99@gmail.com>
2025-06-06 10:08:42 +0530

    Usage limit rectified

--- lib/ai/entitlements.ts
+++ lib/ai/entitlements.ts
@@ -19,7 +19,7 @@ export const entitlementsByUserType: Record<UserType, Entitlements> = {
    * For users with an account
    */
   regular: {
-    maxMessagesPerDay: 50,
+    maxMessagesPerDay: 5,
     availableChatModelIds: chatModels.map(item => item.id),
   },
 
